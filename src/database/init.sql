f60c1c5c6ea58d1b4c1cc15eafc767ba0bf08bd5
Lucas <lduncan1712@gmail.com>
2026-05-14 13:57:07 -0400

    Adding Transactions/Budgets/Goals To DB

--- src/database/init.sql
+++ src/database/init.sql
@@ -1,3 +1,5 @@
+-- ACTORS
+-- ##############################################
 CREATE TABLE IF NOT EXISTS users(
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
@@ -8,6 +10,9 @@ CREATE TABLE IF NOT EXISTS entities(
     name VARCHAR(255) NOT NULL
 );
 
+
+-- SUPPORT
+-- #############################################
 CREATE TYPE durations AS ENUM (
     'DAILY',
     'WEEKLY',
@@ -16,6 +21,37 @@ CREATE TYPE durations AS ENUM (
 );
 
 
+-- BUDGETS & GOALS
+-- #############################################
+CREATE TABLE IF NOT EXISTS budgets(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    budget_start DATE NOT NULL,
+    budget_end   DATE NOT NULL,
+    budget_duration durations NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+ALTER TABLE budgets ADD CONSTRAINT budget_amount CHECK (amount > 0);
+
+
+CREATE TABLE IF NOT EXISTS goals(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    balance DECIMAL(10, 2) NOT NULL,
+    goal_date DATE NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
+
+
+
+-- TRANSACTIONS
+-- #############################################
 CREATE TABLE IF NOT EXISTS transaction_kinds(
     id INTEGER PRIMARY KEY,
     name VARCHAR(255) NOT NULL
@@ -26,7 +62,28 @@ CREATE TABLE IF NOT EXISTS transaction_categories(
     name VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS transactions(
+    id SERIAL PRIMARY KEY,
+    kind_id INTEGER NOT NULL,
+    category_id INTEGER NOT NULL,
 
--- SEED DATA COPY
+    user_id INTEGER NOT NULL,
+    entity_id INTEGER NOT NULL,
+
+    amount DECIMAL(10, 2) NOT NULL,
+    transaction_date DATE NOT NULL,
+
+    FOREIGN KEY (kind_id) REFERENCES transaction_kinds(id),
+    FOREIGN KEY (category_id) REFERENCES transaction_categories(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (entity_id) REFERENCES entities(id)
+);
+
+ALTER TABLE transactions ADD CONSTRAINT transaction_amount CHECK (amount > 0);
+
+
+
+-- SEED DATA
+-- ######################################
 COPY transaction_kinds (id, name) FROM '/docker-entrypoint-initdb.d/seed/transaction_kinds.csv' WITH (FORMAT CSV, HEADER true);
 COPY transaction_categories (id, name) FROM '/docker-entrypoint-initdb.d/seed/transaction_categories.csv' WITH (FORMAT CSV, HEADER true);
