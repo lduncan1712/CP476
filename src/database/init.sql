28a90fca407230620f236947d40a1d9fb324d53b
Lucas <lduncan1712@gmail.com>
2026-06-02 14:42:51 -0400

    Simplifying Schema To User+Entity+Transaction+Budget (4) Tables

--- src/database/init.sql
+++ src/database/init.sql
@@ -1,3 +1,19 @@
+CREATE TYPE durations AS ENUM (
+    'DAILY',
+    'WEEKLY',
+    'MONTHLY',
+    'YEARLY',
+    'DECADE'
+);
+
+CREATE TABLE IF NOT EXISTS transaction_categories(
+    id                     INTEGER PRIMARY KEY,
+    name                   VARCHAR(255) NOT NULL
+);
+
+
+
+
 CREATE TABLE IF NOT EXISTS users(
     id                     SERIAL PRIMARY KEY,
     name                   VARCHAR(255) NOT NULL,
@@ -11,40 +27,14 @@ CREATE TABLE IF NOT EXISTS entities(
     name                   VARCHAR(255) NOT NULL
 );
 
-CREATE TYPE durations AS ENUM (
-    'DAILY',
-    'WEEKLY',
-    'MONTHLY',
-    'YEARLY',
-    'DECADE'
-);
-
-CREATE TABLE IF NOT EXISTS transaction_kinds(
-    id                     INTEGER PRIMARY KEY,
-    name                   VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS transaction_categories(
-    id                     INTEGER PRIMARY KEY,
-    name                   VARCHAR(255) NOT NULL
-);
-
-
 CREATE TABLE IF NOT EXISTS transactions(
-    /*
-        OUT:      (user -> entity),
-        IN:       (entity -> user),
-        TRANSFER: (user -> ), (-> user)    
-    */
     id                     SERIAL PRIMARY KEY,
-    kind_id                INTEGER NOT NULL,
     category_id            INTEGER NOT NULL,
     user_id                INTEGER NOT NULL,
     entity_id              INTEGER NOT NULL,
     amount                 DECIMAL(10, 2) NOT NULL,
     transaction_date       DATE NOT NULL,
 
-    FOREIGN KEY (kind_id)     REFERENCES transaction_kinds(id),
     FOREIGN KEY (category_id) REFERENCES transaction_categories(id),
     FOREIGN KEY (user_id)     REFERENCES users(id),
     FOREIGN KEY (entity_id)   REFERENCES entities(id),
@@ -67,28 +57,13 @@ CREATE TABLE IF NOT EXISTS budgets(
     CONSTRAINT budget_positive CHECK (amount > 0)
 );
 
-CREATE TABLE IF NOT EXISTS goals(
-    id                     SERIAL PRIMARY KEY,
-    user_id                INTEGER NOT NULL,
-    balance                DECIMAL(10, 2) NOT NULL,
-    goal_date              DATE NOT NULL,
-
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
 
 
-CREATE OR REPLACE FUNCTION goal_updates_user()
-RETURNS TRIGGER AS $$
-BEGIN
-    IF TG_OP = 'DELETE' THEN
-        UPDATE users SET updated_on = CURRENT_DATE WHERE id = OLD.user_id;
-        RETURN OLD;
-    ELSE
-        UPDATE users SET updated_on = CURRENT_DATE WHERE id = NEW.user_id;
-        RETURN NEW;
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
+
+
+
+
+
 
 
 CREATE OR REPLACE FUNCTION budget_updates_user()
@@ -108,7 +83,11 @@ CREATE OR REPLACE FUNCTION transaction_updates_user()
 RETURNS TRIGGER AS $$
 BEGIN
     IF TG_OP = 'DELETE' THEN
-        UPDATE users SET updated_on = CURRENT_DATE WHERE id = OLD.user_id;
+        UPDATE users 
+        SET 
+            updated_on = CURRENT_DATE 
+            --current_to = stale? DESIGN DECISION: Push Back On Delete????
+        WHERE id = OLD.user_id;
         RETURN OLD;
     ELSE
         UPDATE users
@@ -123,18 +102,14 @@ $$ LANGUAGE plpgsql;
 
 
 
-CREATE TRIGGER trg_budgets_update_user
+CREATE OR REPLACE TRIGGER trg_budgets_update_user
     AFTER INSERT OR UPDATE OR DELETE ON budgets
     FOR EACH ROW EXECUTE FUNCTION budget_updates_user();
 
-CREATE TRIGGER trg_goals_update_user
-    AFTER INSERT OR UPDATE OR DELETE ON goals
-    FOR EACH ROW EXECUTE FUNCTION goal_updates_user();
 
-CREATE TRIGGER trg_transactions_update_user
+CREATE OR REPLACE TRIGGER trg_transactions_update_user
     AFTER INSERT OR UPDATE OR DELETE ON transactions
     FOR EACH ROW EXECUTE FUNCTION transaction_updates_user();
 
 
-COPY transaction_kinds (id, name) FROM '/docker-entrypoint-initdb.d/seed/transaction_kinds.csv' WITH (FORMAT CSV, HEADER true);
 COPY transaction_categories (id, name) FROM '/docker-entrypoint-initdb.d/seed/transaction_categories.csv' WITH (FORMAT CSV, HEADER true);
