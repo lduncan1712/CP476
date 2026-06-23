39afca07b17006aa541f6870ad1b4f2cc1f4a310
OMacgowan <omacgowan@gmail.com>
2026-06-23 14:11:54 -0400

    Adding BudgetCard contents to .css and .jsx files + an example added in landing.jsx

--- src/frontend/src/components/Landing.jsx
+++ src/frontend/src/components/Landing.jsx
@@ -1,3 +1,16 @@
+import BudgetCard from "./BudgetCard";
+
 export default function Landing() {
-  return <div>Landing Page</div>
+  return (
+    <div>
+      <h1>Landing Page</h1>
+      <BudgetCard 
+        category="Groceries" 
+        amount={500} 
+        spent={300} 
+        days_left={10} 
+      />
+    </div>
+  )
 }
+
