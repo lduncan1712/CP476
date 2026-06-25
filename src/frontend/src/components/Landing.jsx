93fb628c6ec3b96a9e4ae8b1dc0a264421b47bee
Jamie Beatty <jpmbeatty@gmail.com>
2026-06-25 13:25:37 -0400

    Hardcoded landing page

--- src/frontend/src/components/Landing.jsx
+++ src/frontend/src/components/Landing.jsx
@@ -1,16 +1,35 @@
 import BudgetCard from "./BudgetCard";
+import "./Landing.css";
 
 export default function Landing() {
   return (
-    <div>
-      <h1>Landing Page</h1>
-      <BudgetCard 
-        category="Groceries" 
-        amount={500} 
-        spent={300} 
-        days_left={10} 
-      />
-    </div>
-  )
-}
+    <div className="landing-container">
 
+      <div className="welcome-box">
+        <h2>Hello Bob!</h2>
+      </div>
+
+      <div className="date-box">
+        <h2>Date: Monday, May 25th, 2026</h2>
+      </div>
+
+      <div className="overall-budget">
+        <BudgetCard
+          category="Overall Spending"
+          amount={1000}
+          spent={600}
+          days_left={4}
+        />
+      </div>
+
+      <div className="transactions-placeholder">
+        {/* Table here */}
+      </div>
+
+      <BudgetCard category="Groceries" amount={250} spent={171} days_left={4} />
+      <BudgetCard category="Transportation" amount={100} spent={98} days_left={4} />
+      <BudgetCard category="Entertainment" amount={150} spent={173} days_left={4} />
+
+    </div>
+  );
+}
