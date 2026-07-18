b2cd0ccd414144fb9aa24b1dcba964033b7f9b72
Charlie Carter <csc115@outlook.com>
2026-07-18 16:18:21 -0400

    Added routing. May have to restart docker and/or run "npm install react-router-dom" for everything to work.

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import { useState } from 'react'
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 
 //PAGES
 import Login from './pages/Login'
 import MainPage from './pages/MainPage'
 import {CategoriesProvider} from "./contexts/CategoryContext.jsx";
+import Landing from "./components/Landing.jsx";
+import Transactions from "./components/Transactions.jsx";
+import Budgets from "./components/Budgets.jsx";
+import Analytics from "./components/Analytics.jsx";
 
 export default function App() {
   const [loggedIn, setLoggedIn] = useState(false)
@@ -14,9 +19,27 @@ export default function App() {
 
   return (
 
-          <CategoriesProvider>
-              <MainPage />
-          </CategoriesProvider>
+    <CategoriesProvider>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Navigate to="/dashboard"/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route
+                    path="/dashboard/*"
+                    element={<MainPage/>}
+                >
+                    <Route path="" element={<Navigate to="home" replace/>}/>
+                    <Route path="home" element={<Landing/>}/>
+                    <Route path="transactions" element={<Transactions/>}/>
+                    <Route path="budgets" element={<Budgets/>}/>
+                    <Route path="analytics" element={<Analytics/>}/>
+                </Route>
+            </Routes>
+
+        </BrowserRouter>
+
+    </CategoriesProvider>
+
 
   )
 
--- src/frontend/src/pages/MainPage.jsx
+++ src/frontend/src/pages/MainPage.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './MainPage.css'
 
 //COMPONENTS CONDITIONALLY SHOWN WITHIN
@@ -6,32 +5,35 @@ import Landing from '../components/Landing'
 import Transactions from '../components/Transactions'
 import Budgets from '../components/Budgets'
 import Analytics from '../components/Analytics'
-
+import {Link as RouterLink, Outlet, useLocation, useNavigate} from "react-router-dom";
 
 const TABS = [
-  { label: 'Landing', component: <Landing /> },
-  { label: 'Transaction', component: <Transactions /> },
-  { label: 'Budgets', component: <Budgets /> },
-  { label: 'Analytics', component: <Analytics /> },
+  { label: 'Landing', component: <Landing />, path: '/dashboard/home' },
+  { label: 'Transaction', component: <Transactions />, path: '/dashboard/transactions' },
+  { label: 'Budgets', component: <Budgets />, path: '/dashboard/budgets' },
+  { label: 'Analytics', component: <Analytics />, path: '/dashboard/analytics' },
   // { label: 'Settings', component: <Settings /> },
 ]
 
 export default function MainPage() {
-  const [activeTab, setActiveTab] = useState(0)
+  const { pathname } = useLocation()
 
   return (
     <div className="main-page-wrapper">
       <div className="top-bar">The Budget App</div>
       <div className="main-page">
         <nav>
-          {TABS.map((tab, i) => (
-            <button key={tab.label} onClick={() => setActiveTab(i)} disabled={activeTab === i}>
-              {tab.label}
-            </button>
+          {TABS.map((tab) => (
+            <RouterLink key={tab.label} to={tab.path}>
+              <button disabled={pathname === tab.path}>
+                {tab.label}
+              </button>
+            </RouterLink>
           ))}
         </nav>
         <main>
-          {TABS[activeTab].component}
+
+          <Outlet/>
         </main>
       </div>
     </div>
